28f4c142d471fa597b29e29d402a993629f46456
Rizey777 <rifkirizey@gmail.com>
2025-03-06 23:22:22 -0800

    initial codes

---
+++ praktikum2-Registrasi/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+
+Route::view('/register', 'register');
+Route::POST('/submit',[RegisterController::class,'submit']);
+Route::get('/', function () {
+    return view('welcome');
+});
